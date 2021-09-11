f96cd57148f3e5f5934dfa93f2f6963449328aa4
SokolOFFF <egor1sokolov@gmail.com>
2021-09-11 17:22:05 +0300

    added some buttons and links to theory

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 TOKEN = "1963238350:AAE4CcFX1tURWjzdXX9awKa_oxmYXFa5sKM"
 HelpSticker = "https://t.me/stikertforlinal/2"
 StartSticker = "https://t.me/stikertforlinal/3"
+themes = ["Dot Product", "Cross Product", "Matrix"]
+linksToThemes = ["https://en.wikipedia.org/wiki/Dot_product",
+                 "https://en.wikipedia.org/wiki/Cross_product",
+                 "https://en.wikipedia.org/wiki/Matrix_(mathematics)"]
--- main.py
+++ main.py
@@ -1,23 +1,71 @@
 import telebot
 import config
 
+from telebot import types
+
 bot = telebot.TeleBot(config.TOKEN)
+
+theoryAndCalculatingMarkup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+theoryButton = types.KeyboardButton("Theory")
+calcButton = types.KeyboardButton("Calculating")
+theoryAndCalculatingMarkup.add(theoryButton, calcButton)
 
 @bot.message_handler(content_types=['text'], commands=['start', 'help'])
 def handle_start_help(message):
+
+
+
     if message.text == "/start":
         bot.send_sticker(message.chat.id, data=config.StartSticker)
+        bot.send_message(message.chat.id,
+                         text="Hello, {0.first_name}!\nI am <b>{1.first_name}</b>, and I am <b>LinAl bot</b>. \nI hope I can help you somehow. ❤️".format(
+                             message.from_user, bot.get_me()), parse_mode='HTML', reply_markup=theoryAndCalculatingMarkup)
     if message.text == "/help":
         bot.send_sticker(message.chat.id, data=config.HelpSticker)
-    bot.send_message(message.chat.id, text="Hello! Im am Linear Algebra helper! Here you can solve several Linal problems! If something does not work, contact to @yeawou! Good luck!")
+        bot.send_message(message.chat.id, text="Use buttons to navigate!", reply_markup=theoryAndCalculatingMarkup)
+
 
 def test1(message):
     bot.send_message(message.chat.id, message.text)
 
 
 @bot.message_handler(content_types=['text'])
-def linal(message):
-    bot.send_message(message.chat.id, text="salam")
+def messageChecker(message):
+    if message.chat.type == 'private':
+        if message.text == 'Theory':
+            theory(message)
+        elif message.text == "Calculating":
+            calculating(message)
+        else:
+            bot.send_message(message.chat.id, text="Sorry, unknown command, please, check your input.")
+
+def theory(message):
+
+    themesMarkup = types.InlineKeyboardMarkup(row_width=1)
+    themes = config.themes
+    for i in range(len(themes)):
+        item = types.InlineKeyboardButton(themes[i], callback_data=str(i))
+        themesMarkup.add(item)
+
+    bot.send_message(message.chat.id, text="Here are some themes. Select anything:", reply_markup=themesMarkup)
+
+def calculating(message):
+    bot.send_message(message.chat.id, text="Calculating in progress...")
+
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline(call):
+    try:
+        if call.message:
+            for i in range(len(config.themes)):
+                if call.data == str(i):
+                    bot.send_message(call.message.chat.id, config.linksToThemes[i])
+            bot.edit_message_text(chat_id=call.message.chat.id,
+                                  message_id=call.message.message_id,
+                                  text="Here are some themes. Select anything:",
+                                  reply_markup=None)
+    except Exception as e:
+        print(repr(e))
 
 
 bot.polling(none_stop=True)
